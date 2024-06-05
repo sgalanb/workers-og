683c94fa7f36a7aad2f137a1a71b63279a98a824
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-06-05 18:27:07 -0300

    fix

--- packages/workers-og/src/parseHtml.ts
+++ packages/workers-og/src/parseHtml.ts
@@ -17,35 +17,42 @@ import {
  */
 export async function parseHtml(html: string): Promise<React.ReactNode | null> {
   let vdomStr = ``;
+  let insideElement = false;
 
   const rewriter = new HTMLRewriter()
     .on("*", {
       element(element: Element) {
+        insideElement = true;
         const attrs = getAttributes(element);
         vdomStr += `{"type":"${element.tagName}", "props":{${attrs}"children": [`;
         try {
           element.onEndTag(() => {
             vdomStr = maybeRemoveTrailingComma(vdomStr);
             vdomStr += `]}},`;
+            insideElement = false;
           });
         } catch (e) {
           vdomStr = maybeRemoveTrailingComma(vdomStr);
           vdomStr += `]}},`;
+          insideElement = false;
         }
       },
       text(text: Text) {
         if (text.text) {
           const sanitized = sanitizeJSON(text.text);
           if (sanitized) {
-            vdomStr += `"${sanitized}",`;
+            if (insideElement) {
+              vdomStr += `"${sanitized}",`;
+            } else {
+              vdomStr += `"${sanitized}"`;
+            }
           }
         }
       },
     })
     .transform(
       new Response(
-        // Add a parent to ensure that we're only dealing
-        // with a single root element
+        // Ensure to add a parent to handle multiple roots
         `<div style="display: flex; flex-direction: column;">${html}</div>`
       )
     );
