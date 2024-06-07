777c45eb82496bd6f1492e495ff933cf6031cef1
sgalanb <73245438+sgalanb@users.noreply.github.com>
2024-06-06 21:06:34 -0300

    release v1

--- packages/workers-og/src/parseHtml.ts
+++ packages/workers-og/src/parseHtml.ts
@@ -55,14 +55,10 @@ export async function parseHtml(html: string): Promise<React.ReactNode | null> {
 
   vdomStr = maybeRemoveTrailingComma(vdomStr);
 
-  console.log(JSON.parse(vdomStr));
-
   let finalJson = JSON.parse(vdomStr);
   // If an element has only one children and it is a string then
   // replace the children array with the string itself
   simplifyChildren(finalJson);
-
-  console.log(finalJson);
 
   try {
     return finalJson;
